96dc8dc8dc18dc56c8bb603df401f5db97eb66cb
David masterson <david73180@gmail.com>
2018-09-17 00:27:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'pry'
-def fizzbuzz(arg1)
-  if arg1 % 3 == 0 
-    return "fizz"
-  elsif arg1 % 5 == 0 
-    return "buzz"
-  elsif arg1 % 3 == 0 && arg1 % 5 == 0 
-    return "fizzbuzz"
-  else
-    return "nil"
-  binding.pry
+
+def fizzbuzz
 
 end
